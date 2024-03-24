a105a3d1cb5d5be9f19725afc394bd3554739c0a
Shazia Nazir <125131505+shaziamughal@users.noreply.github.com>
2024-03-24 12:56:52 +0500

    Create Labtask1.asm
    Lab 1 task: This code shows how we can move values to registers in assembly language.

---
+++ Labtask1.asm
@@ -0,0 +1,16 @@
+Include irvine32.inc
+.data
+;variable declarations go here
+.code
+Main Proc
+;instructions go here
+Mov 	eax, 30h		;Assembly Language is NOT case sensitive
+Mov 	ebx, 20h
+mov	ecx, 0h
+
+Add 	ecx, eax
+Add 	ecx, ebx
+Call 	dumpregs	;displays the result on the screen by displaying all register values
+Exit
+Main endp
+End main
